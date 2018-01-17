37b1371f4f9d8c392f5ac5c447c0e73a7cc26ad4
Ryan McCrystal <rrmccrystal@gmail.com>
2018-01-17 10:09:12 -1000

    Added some TODOs for next time

--- src/mccrystal/ryan/Entity.java
+++ src/mccrystal/ryan/Entity.java
@@ -24,6 +24,8 @@ public abstract class Entity {
 
     protected World currentWorld;
 
+    protected Rectangle rectangle;
+
     public Entity(float positionX, float positionY, float width, float height, Color color) {
         this.positionX = positionX;
         this.positionY = positionY;
@@ -48,7 +50,8 @@ public abstract class Entity {
         for(Entity e : getWorld().getEntityList()) {
             if(e instanceof Ground) {
                 Ground ground = (Ground) e;
-                if(new Rectangle((int) positionX, (int) positionY, (int) width, (int) height).contains(ground.positionX, ground.positionY, ground.width, ground.height)) {
+                Rectangle hisRectangle = new Rectangle((int) positionX, (int) positionY, (int) width, (int) height
+                if(new Rectangle((int) positionX, (int) positionY, (int) width, (int) height).contains(ground.positionX, ground.positionY, ground.width, ground.height)) { //TODO: This checks if the ground COMPLETELY contains the object. You need to create your own function to check if two rectangles are touching
                     return true;
                 }
             }
@@ -66,13 +69,13 @@ public abstract class Entity {
         positionY += velocityY;
         positionX += velocityX;
         if(hasGravity && !onGround) {
-            velocityY += currentWorld.getGravitiy(); //TODO: Get this working
+            velocityY += currentWorld.getGravitiy();
         }
     }
 
     public void render(Graphics2D graphics) {
         graphics.setColor(color);
-        graphics.fillRect((int) positionX, (int) positionY, (int) width, (int) height);
+        graphics.fillRect((int) positionX, (int) positionY, (int) width, (int) height); //TODO: Replace this with a rectangle object and use graphics.draw(shape)
     }
 
 
